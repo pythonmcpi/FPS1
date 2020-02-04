9b8b9047c37a9850fc20944c7f062d3e39269bdc
Python Minecraft Raspberry Pi <47196755+pythonmcpi@users.noreply.github.com>
2020-02-04 12:07:55 -0800

    Update main.py

--- main.py
+++ main.py
@@ -27,3 +27,46 @@ screen = Screen(screenSize, (100,100), "FPS1")
 # And screen.after(function,timeout,*args) calls tkinter's after()
 # I also added a screen.drawcircle()
 
+tickSpeed = 50
+
+def getNewRelativePos(speed, angle):
+    return [speed*math.cos(angle),speed*math.sin(angle)]
+
+def getBounds():
+    return screenSize[:]
+
+class Object:
+    def __init__(self, pos):
+        self.x = pos[0]
+        self.y = pos[1]
+
+class Moveable(Object): # Name comes from the 'Moveable' class from Tanks.jar
+    def __init__(self, pos, angle, speed):
+        super().__init__(pos)
+        self.angle = angle
+        self.speed = speed
+
+    def tick(self):
+        xOffset, yOffset = getNewRelativePos(self.speed, self.angle)
+        self.x, self.y += xOffset, yOffset
+
+class Bullet(Moveable):
+    def __init(self, pos, angle, speed):
+        super().__init__(pos)
+
+    def tick(self):
+        super().tick()
+        if not self.checkBounds():
+            self.bounce()
+
+    def checkBounds(self): # Need to update this functions when I implement Obstacles
+        boundX, boundY = getBounds()
+        if not 0 =< self.x =< boundX: # If speed is too fast, it will be glitchy, bullets will not bounce properly. Tickspeed must be pretty high so I can keep speed low, but if tickspeed is too fast the game will lag severely.
+            return False
+        elif not 0 =< self.y =< boundY:
+            return False
+        else:
+            return True
+
+    def bounce(self):
+        pass
